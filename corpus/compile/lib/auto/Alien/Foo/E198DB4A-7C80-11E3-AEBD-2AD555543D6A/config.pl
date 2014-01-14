5ad344d4bcb5a176b65dc150a9f889c5c9e85b0c
Graham Ollis <plicease@cpan.org>
2014-01-14 14:15:45 -0500

    the strawberry regression

--- corpus/compile/lib/auto/Alien/Foo/E198DB4A-7C80-11E3-AEBD-2AD555543D6A/config.pl
+++ corpus/compile/lib/auto/Alien/Foo/E198DB4A-7C80-11E3-AEBD-2AD555543D6A/config.pl
@@ -1,7 +1,7 @@
 {
   cflags    => "-I%d/include -DFOO=42",
   # Question: can/should this transformation happen automatically in Alien::Xenomorph
-  libs      => ($^O ne 'MSWin32' && $Config::Config{cc} ? "-L%d/lib -lfoo" : "-LIBPATH:%d/lib foo.lib"),
+  libs      => ($^O ne 'MSWin32' || $Config::Config{cc} !~ /cl(\.exe)?$/ ? "-L%d/lib -lfoo" : "-LIBPATH:%d/lib foo.lib"),
   version   => '1.00',
   timestamp => 123456789,
 }
